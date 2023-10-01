e9ee3d27a29d988d97e2946e3bc3866e8aee5e12
seadynamic8 <seadynamic8@zoho.com>
2023-09-30 20:28:29 -0400

    feat: added logout to chat screen

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:chat_app2_supabase/utils/constants.dart';
+
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
 
@@ -8,11 +10,21 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  void _logout() async {
+    await supabase.auth.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Chat'),
+        actions: [
+          IconButton(
+            onPressed: _logout,
+            icon: const Icon(Icons.logout),
+          ),
+        ],
       ),
       body: const Text('Chat Area'),
     );
