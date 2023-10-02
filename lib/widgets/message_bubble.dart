335907cbd7b01580113ed9bc27127075a03c49cd
seadynamic8 <seadynamic8@zoho.com>
2023-10-01 22:08:24 -0400

    feat: added some styling to the message bubble to look nicer for the text and bubble backgrounds

--- lib/widgets/message_bubble.dart
+++ lib/widgets/message_bubble.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:chat_app2_supabase/utils/constants.dart';
 
-class MessageBubble extends StatefulWidget {
+class MessageBubble extends StatelessWidget {
   const MessageBubble({
     super.key,
     required this.username,
@@ -18,27 +18,24 @@ class MessageBubble extends StatefulWidget {
   final String createdAt;
   final bool isFirstInSequence;
 
-  @override
-  State<MessageBubble> createState() => _MessageBubbleState();
-}
-
-class _MessageBubbleState extends State<MessageBubble> {
   bool get isMe {
     final currentUserId = supabase.auth.currentUser!.id;
-    return widget.userId == currentUserId;
+    return userId == currentUserId;
   }
 
   @override
   Widget build(BuildContext context) {
+    final themeContext = Theme.of(context);
+
     return Column(
       crossAxisAlignment:
           isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
       children: [
-        if (widget.username != null && widget.isFirstInSequence)
+        if (username != null && isFirstInSequence)
           Padding(
             padding: const EdgeInsets.only(left: 13, right: 13),
             child: Text(
-              widget.username!,
+              username!,
               style: const TextStyle(
                 color: Colors.black87,
                 fontWeight: FontWeight.bold,
@@ -47,13 +44,32 @@ class _MessageBubbleState extends State<MessageBubble> {
           ),
         Container(
           decoration: BoxDecoration(
-            color: Colors.grey[300],
-            borderRadius: BorderRadius.circular(12),
+            color: isMe
+                ? Colors.grey[300]
+                : themeContext.colorScheme.secondary.withAlpha(200),
+            borderRadius: BorderRadius.only(
+              topLeft: !isMe && isFirstInSequence
+                  ? Radius.zero
+                  : const Radius.circular(12),
+              topRight: isMe && isFirstInSequence
+                  ? Radius.zero
+                  : const Radius.circular(12),
+              bottomLeft: const Radius.circular(12),
+              bottomRight: const Radius.circular(12),
+            ),
           ),
           constraints: const BoxConstraints(maxWidth: 200),
           padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 14),
           margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 12),
-          child: Text(widget.content),
+          child: Text(
+            content,
+            style: TextStyle(
+              height: 1.3,
+              color:
+                  isMe ? Colors.black87 : themeContext.colorScheme.onSecondary,
+            ),
+            softWrap: true,
+          ),
         ),
       ],
     );
